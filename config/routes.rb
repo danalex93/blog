eac60746eb96a9b2abb18bb6550e241bcdfd2469
Daniel Morales Miguel <daniel.morales.miguel@gmail.com>
2013-10-11 16:19:30 -0300

    creating Works page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blog::Application.routes.draw do
+  get "works/index"
   get 'admin', to: 'admin#index'
   get 'about', to: 'about#index'
+  get 'works', to: 'works#index'
   resources :posts
 
   get "welcome/index"
