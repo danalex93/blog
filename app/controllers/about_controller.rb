06dd75a20ee4e1caf0e7446d58681311f42a29cf
Daniel Morales Miguel <daniel.morales.miguel@gmail.com>
2013-10-29 13:46:25 -0300

    Adding link to posts

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,7 +1,5 @@
 class AboutController < ApplicationController
-  
-  http_basic_authenticate_with name: "admin", password: "supersecretpassword"
-
+	
   def index
   end
   
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "supersecretpassword"
   def index
   	@posts = Post.all
   end
