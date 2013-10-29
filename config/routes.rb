0d390c3fda793ccb8dca23f8ec2fe3d56f807eb5
Daniel Morales Miguel <daniel.morales.miguel@gmail.com>
2013-10-29 15:33:42 -0400

    New comments feature 0.1

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Blog::Application.routes.draw do
   get 'works', to: 'works#index'
   resources :posts
 
+  resources :posts do
+    resources :comments
+  end
+
   get "welcome/index"
   root 'welcome#index'
 
-end
-
-resources :posts do
-  resources :comments
 end
