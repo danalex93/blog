4031d22afdf4df8b5f137f580de9181dc0e9c642
Daniel Morales Miguel <daniel.morales.miguel@gmail.com>
2013-11-05 16:52:26 -0300

    Effect on click in CV page

--- public/js/animations.js
+++ public/js/animations.js
@@ -9,8 +9,8 @@ init = function(){
       jarallax.addAnimation('.planet0',[{progress: "0%", top:"0%"}, {progress: "100%", top: "-10%"}]);
       
       // 0 - 20
-      jarallax.addAnimation('#head1',[{progress: "0%", left:"-800px"}, {progress: "10%", left: "100px"}]);
-      jarallax.addAnimation('#head1',[{progress: "10%", left:"100px"}, {progress: "20%", left: "150px"}]);
+      jarallax.addAnimation('#head1',[{progress: "0%", left:"-800px"}, {progress: "5%", left: "100px"}]);
+      jarallax.addAnimation('#head1',[{progress: "5%", left:"100px"}, {progress: "20%", left: "150px"}]);
       jarallax.addAnimation('#head1',[{progress: "0%", opacity:"0"}, {progress: "10%", opacity:"1"}]);
       jarallax.addAnimation('#head1',[{progress: "10%", opacity:"1"}, {progress: "20%", opacity:"0"}]);
       
@@ -20,8 +20,8 @@ init = function(){
       jarallax.addAnimation('#p1',[{progress: "0%", marginLeft:"0"}, {progress: "20%"}]);
       
       // 20 - 40
-      jarallax.addAnimation('#head2',[{progress: "20%", left:"-800px"}, {progress: "30%", left: "100px"}]);
-      jarallax.addAnimation('#head2',[{progress: "30%", left:"100px"}, {progress: "40%", left: "150px"}]);
+      jarallax.addAnimation('#head2',[{progress: "20%", left:"-800px"}, {progress: "25%", left: "100px"}]);
+      jarallax.addAnimation('#head2',[{progress: "25%", left:"100px"}, {progress: "40%", left: "150px"}]);
       jarallax.addAnimation('#head2',[{progress: "20%", opacity:"1"}, {progress: "30%", opacity:"1"}]);
       jarallax.addAnimation('#head2',[{progress: "30%", opacity:"1"}, {progress: "40%", opacity:"0"}]);
       
@@ -31,8 +31,8 @@ init = function(){
       jarallax.addAnimation('#p2',[{progress: "20%", marginLeft:"0"}, {progress: "40%"}]);
       
       // 40 - 60
-      jarallax.addAnimation('#head3',[{progress: "40%", left:"-800px"}, {progress: "50%", left: "100px"}]);
-      jarallax.addAnimation('#head3',[{progress: "50%", left:"100px"}, {progress: "60%", left: "150px"}]);
+      jarallax.addAnimation('#head3',[{progress: "40%", left:"-800px"}, {progress: "45%", left: "100px"}]);
+      jarallax.addAnimation('#head3',[{progress: "45%", left:"100px"}, {progress: "60%", left: "150px"}]);
       jarallax.addAnimation('#head3',[{progress: "40%", opacity:"1"}, {progress: "50%", opacity:"1"}]);
       jarallax.addAnimation('#head3',[{progress: "50%", opacity:"1"}, {progress: "60%", opacity:"0"}]);
       
@@ -42,8 +42,8 @@ init = function(){
       jarallax.addAnimation('#p3',[{progress: "40%", marginLeft:"0"}, {progress: "60%"}]);     
       
       // 60 - 80
-      jarallax.addAnimation('#head4',[{progress: "60%", left:"-800px"}, {progress: "70%", left: "100px"}]);
-      jarallax.addAnimation('#head4',[{progress: "70%", left:"100px"}, {progress: "80%", left: "150px"}]);
+      jarallax.addAnimation('#head4',[{progress: "60%", left:"-800px"}, {progress: "65%", left: "100px"}]);
+      jarallax.addAnimation('#head4',[{progress: "65%", left:"100px"}, {progress: "80%", left: "150px"}]);
       jarallax.addAnimation('#head4',[{progress: "60%", opacity:"1"}, {progress: "70%", opacity:"1"}]);
       jarallax.addAnimation('#head4',[{progress: "70%", opacity:"1"}, {progress: "80%", opacity:"0"}]);
       
@@ -53,8 +53,8 @@ init = function(){
       jarallax.addAnimation('#p4',[{progress: "60%", marginLeft:"0"}, {progress: "80%"}]);     
       
       // 80 - 100
-      jarallax.addAnimation('#head5',[{progress: "80%", left:"-800px"}, {progress: "90%", left: "100px"}]);
-      jarallax.addAnimation('#head5',[{progress: "90%", left:"100px"}, {progress: "100%", left: "150px"}]);
+      jarallax.addAnimation('#head5',[{progress: "80%", left:"-800px"}, {progress: "85%", left: "100px"}]);
+      jarallax.addAnimation('#head5',[{progress: "85%", left:"100px"}, {progress: "100%", left: "150px"}]);
       jarallax.addAnimation('#head5',[{progress: "80%", opacity:"1"}, {progress: "100%", opacity:"1"}]);
       
       jarallax.addAnimation('#p5',[{progress: "80%", opacity:"0"}, {progress: "85%", opacity:"1"}]);
---
+++ public/script.js
@@ -0,0 +1,36 @@
+// Code goes here
+
+jQuery(function( $ ){
+  		/**
+			 * Demo binding and preparation, no need to read this part
+			 */
+				//borrowed from jQuery easing plugin
+				//http://gsgd.co.uk/sandbox/jquery.easing.php
+				$.easing.elasout = function(x, t, b, c, d) {
+					var s=1.70158;var p=0;var a=c;
+					if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+					if (a < Math.abs(c)) { a=c; var s=p/4; }
+					else var s = p/(2*Math.PI) * Math.asin (c/a);
+					return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+				};
+			
+			// This one is important, many browsers don't reset scroll on refreshes
+			// Reset all scrollable panes to (0,0)
+			$('div.pane').scrollTo( 0 );
+			// Reset the screen to (0,0)
+			$.scrollTo( 0 );
+			
+			// adding a click handler for each link within the div with the id nav-list
+			$('#menu li a').click(function(){//$.scrollTo works EXACTLY the same way, but scrolls the whole screen
+				$.scrollTo( this.hash, 1000, 0);
+				return false;
+			});
+
+    		// adding a click handler for the link at the bottom
+			$('h2.toNav').click(function(){//$.scrollTo works EXACTLY the same way, but scrolls the whole screen
+				var scrollTargetId = this.hash;
+				$.scrollTo( scrollTargetId, 1000, { easing:'elasout' });				
+				return false;
+			});			
+
+		});
